2047552455f9e294bf45b03db084b14d858a4564
Sunggeun Park <sunggeun.park@gmail.com>
2023-09-27 19:36:13 +0900

    Add feature to draw acc_x graph

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog
 from PyQt5.QtGui import QIcon
 import pandas as pd
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
 
 
 class MyApp(QMainWindow):
@@ -10,8 +12,8 @@ class MyApp(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.textEdit = QTextEdit()
-        self.setCentralWidget(self.textEdit)
+        # self.textEdit = QTextEdit()
+        # self.setCentralWidget(self.textEdit)
         self.statusBar()
 
         openFile = QAction(QIcon("open.png"), "Open", self)
@@ -25,22 +27,35 @@ class MyApp(QMainWindow):
         fileMenu.addAction(openFile)
 
         self.setWindowTitle("File Dialog")
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(300, 300, 640, 480)
         self.show()
 
     def showDialog(self):
         fname = QFileDialog.getOpenFileName(self, "Open file", "./")
 
         if fname[0]:
-            sensor_df = pd.read_csv(fname[0], sep=",", header=0)
-            for col in sensor_df.columns:
+            self.sensor_df = pd.read_csv(fname[0], sep=",", header=0)
+            for col in self.sensor_df.columns:
                 print(col.strip())
 
-            f = open(fname[0], "r")
+            self.plot_data()
 
-            with f:
-                data = f.read()
-                self.textEdit.setText(data)
+            # f = open(fname[0], "r")
+
+            # with f:
+            #     data = f.read()
+            #     self.textEdit.setText(data)
+
+    def plot_data(self):
+        self.plot_widget = PlotWidget()
+        self.plot_widget.setLabel("left", "Y축")
+        self.plot_widget.setLabel("bottom", "X축")
+
+        x = self.sensor_df["timestamp"]
+        y = self.sensor_df[" acc_x"]
+
+        self.plot_widget.plot(x, y, pen=pg.mkPen("b"))
+        self.setCentralWidget(self.plot_widget)
 
 
 if __name__ == "__main__":
