5e9bf0127a2ac34ea63b4f222fb9e19fbbdea41c
Sunggeun Park <sunggeun.park@gmail.com>
2023-09-27 19:11:30 +0900

    Read csv file using pandas

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog
 from PyQt5.QtGui import QIcon
+import pandas as pd
 
 
 class MyApp(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -14,32 +14,36 @@ class MyApp(QMainWindow):
         self.setCentralWidget(self.textEdit)
         self.statusBar()
 
-        openFile = QAction(QIcon('open.png'), 'Open', self)
-        openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Open New File')
+        openFile = QAction(QIcon("open.png"), "Open", self)
+        openFile.setShortcut("Ctrl+O")
+        openFile.setStatusTip("Open New File")
         openFile.triggered.connect(self.showDialog)
 
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
-        fileMenu = menubar.addMenu('&File')
+        fileMenu = menubar.addMenu("&File")
         fileMenu.addAction(openFile)
 
-        self.setWindowTitle('File Dialog')
+        self.setWindowTitle("File Dialog")
         self.setGeometry(300, 300, 300, 200)
         self.show()
 
     def showDialog(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', './')
+        fname = QFileDialog.getOpenFileName(self, "Open file", "./")
 
         if fname[0]:
-            f = open(fname[0], 'r')
+            sensor_df = pd.read_csv(fname[0], sep=",", header=0)
+            for col in sensor_df.columns:
+                print(col.strip())
+
+            f = open(fname[0], "r")
 
             with f:
                 data = f.read()
                 self.textEdit.setText(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
